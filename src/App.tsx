cb8371c6351ae6a43d6ac3939571452ada5b371c
Maximilian <scientificresearch309@gmail.com>
2024-05-28 11:02:31 +0200

    pricing element as a new route added!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Product } from "./pages/Product";
+import { Pricing } from "./pages/Pricing";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
       </Routes>
     </BrowserRouter>
   );
