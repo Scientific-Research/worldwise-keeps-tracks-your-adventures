bc3e09fa29188ce518107cc9cef686324a63bc63
Maximilian <scientificresearch309@gmail.com>
2024-05-28 11:05:42 +0200

    home page element as a new route was added!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Product } from "./pages/Product";
 import { Pricing } from "./pages/Pricing";
+import { Home } from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
       </Routes>
