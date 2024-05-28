777259210be770e361f6a295f8629eac4b20bf4e
Maximilian <scientificresearch309@gmail.com>
2024-05-28 12:29:21 +0200

    a tiny comment added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
       {/* the below a href will reload the whole page and therefore, this is not what we want as SPA => Single Page Application! that's why i comment it out and i use the Link from react-router-dom instead! */}
       {/* <a href="/pricing">go to Pricing Page!</a> */}
 
-      {/* Using Link to go to the Pricing page without loading the whole page! */}
+      {/* Using Link to go to the Pricing page without loading the whole page! => making this as SPA => Single Page Application*/}
       <Link to="/pricing">Pricing</Link>
     </>
   );
