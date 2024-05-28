bc09f071529947e5ddc037d2ab58582e19698c2e
Maximilian <scientificresearch309@gmail.com>
2024-05-28 23:57:30 +0200

    logo added and its css file

---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+import styles from "./Logo.module.css";
+
+export const Logo = () => {
+  return <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />;
+};
--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 // using NavLink instead of Link to give us the active class in CSS => class='active' which we can use it later to determine which class we have already selected!!
 import styles from "./PageNav.module.css";
+import { Logo } from "./Logo";
 
 export const PageNav = () => {
   return (
     <nav className={styles.nav}>
+      <Logo></Logo>
       <ul>
         {/* <li>
           <NavLink to="/">Home</NavLink>
