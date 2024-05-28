7296d2a94084b0fc7b2c21e847ee94283144d175
Maximilian <scientificresearch309@gmail.com>
2024-05-28 23:51:55 +0200

    login navlink added to the PageNav.tsx

--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -6,14 +6,17 @@ export const PageNav = () => {
   return (
     <nav className={styles.nav}>
       <ul>
-        <li>
+        {/* <li>
           <NavLink to="/">Home</NavLink>
-        </li>
+        </li> */}
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
