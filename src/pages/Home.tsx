12b4690e071c2a8b6d8c63ddcf5882fb23df8353
Maximilian <scientificresearch309@gmail.com>
2024-05-28 12:27:52 +0200

    using kink to go to the pricing page without loading the whole page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 export const Home = () => {
   return (
     <>
       <div>WorldWise</div>
-      <a href="/pricing">go to Pricing Page!</a>
+      {/* the below a href will reload the whole page and therefore, this is not what we want as SPA => Single Page Application! that's why i comment it out and i use the Link from react-router-dom instead! */}
+      {/* <a href="/pricing">go to Pricing Page!</a> */}
+
+      {/* Using Link to go to the Pricing page without loading the whole page! */}
+      <Link to="/pricing">Pricing</Link>
     </>
   );
 };
