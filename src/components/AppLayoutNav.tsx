d8296a2f933fb35e43472d5c12a8c54d5349d448
Maximilian <scientificresearch309@gmail.com>
2024-05-29 14:27:30 +0200

    implementing the NavLink in AppLayoutNav to switch between diffrent nested Routes => dirrefent URLs

--- src/components/AppLayoutNav.tsx
+++ src/components/AppLayoutNav.tsx
@@ -1,5 +1,18 @@
 import styles from "./AppLayoutNav.module.css";
+import { NavLink } from "react-router-dom";
 
+// to switch between different nested Routes => diffrent URLs, we use the following NavLinks:
 export const AppLayoutNav = () => {
-  return <nav className={styles.nav}>App Navigation</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
