da8cc61c4436fc20048b2ee5947ba55e6398b9cf
Maximilian <scientificresearch309@gmail.com>
2024-05-30 17:53:37 +0200

    using useNavigate() one another use custom hook in react to move to a form without clicking on a link

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,12 @@
 // import { Outlet } from "react-router-dom";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 export const Map = () => {
+  // another hook provided by react-router-dom => we can use navigate to move to any URL.
+  // for example, when i click on any point in the right side of the page(on the map section) => it doesn't matter, what is the current URL => it will change immediately to the following URL path without clicking on any link: http://localhost:5173/app/form
+  const navigate = useNavigate();
+
   // using useSearchParams() to get the lat and lng from URL:
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -10,7 +14,8 @@ export const Map = () => {
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    // after clicking on any point on the right side of the page(Map section) => the URL path will change immediately to this path without clicking on any link: http://localhost:5173/app/form => this is the navigate function. Something similar to the NavLink but without any declaration!
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <h1>
         Position:{lat},{lng}
