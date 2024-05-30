710788bb3c0da138bd379bcaf0a1e9bf5c30589d
Maximilian <scientificresearch309@gmail.com>
2024-05-30 11:07:15 +0200

    using reduce method, i got the unique countries and  send it to the Country Item to show them in the page under countries category

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -1,4 +1,4 @@
-import { CityItem } from "./CityItem";
+import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
@@ -11,10 +11,19 @@ export const CountryList = ({ cities, isLoading }: any) => {
       <Message message="Add your first city by clicking on a city on the map!" />
     );
 
+  // get countries but as unique countries: we take the cities as array with empty initial value, and then loop through this array, when country in this array is not still included, this will be included and when this country is already included in this array, it will not be added in this array anymore and we will have the last array with the unique countries inside it at the end!
+  const countries = cities.reduce((arr: any, city: any) => {
+    if (!arr.map((el: any) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  // console.log("countries " + countries);
+
   return (
     <ul className={styles.countryList}>
-      {cities.map((city: any) => (
-        <CityItem city={city} key={city.id} />
+      {countries.map((country: any, i: any) => (
+        <CountryItem country={country} key={i} />
       ))}
     </ul>
   );
